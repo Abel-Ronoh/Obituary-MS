8a8e84d0834ee9d8ef56363cf2790da225b3e4d9
Abel-Ronoh <abellronoh@gmail.com>
2024-07-19 23:01:00 +0300

    changed type of e

--- src/app/components/Upload/page.tsx
+++ src/app/components/Upload/page.tsx
@@ -1,7 +1,6 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { useRouter } from 'next/navigation';
-
 import { db, storage } from '../../firebase-config';
 import { collection, addDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
@@ -10,10 +9,10 @@ export default function Upload() {
     const router = useRouter();
     const [fullName, setFullName] = useState('');
     const [biography, setBiography] = useState('');
-    const [image, setImage] = useState(null);
+    const [image, setImage] = useState<File | null>(null);
 
-    const handleImageChange = (e) => {
-        if (e.target.files[0]) {
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
             setImage(e.target.files[0]);
         }
     };
@@ -34,7 +33,7 @@ export default function Upload() {
                 image: imageUrl
             });
             console.log("Document written with ID: ", docRef.id);
-            alert("obituary added")
+            alert("Obituary added");
         } catch (e) {
             console.error("Error adding document: ", e);
         }
@@ -42,18 +41,18 @@ export default function Upload() {
 
     return (
         <div className="flex flex-col items-center justify-center h-[100vh] bg-[#344648] text-[#fbe0c3]">
-        <button className="fixed top-4 right-4 w-32 h-16 bg-gray-500 rounded-sm mr-3 text-white hover:bg-pink-700" onClick={() => router.push('../')}>Home</button>
+            <button className="fixed top-4 right-4 w-32 h-16 bg-gray-500 rounded-sm mr-3 text-white hover:bg-pink-700" onClick={() => router.push('../')}>Home</button>
             
-            <div className=' w-[600px] flex flex-col items-center'>
-            <div className="flex flex-col items-center mb-10">
-                    <label htmlFor="fullName">Full Names</label>
+            <div className='w-[600px] flex flex-col items-center'>
+                <div className="flex flex-col items-center mb-10">
+                    <label htmlFor="fullName">Full Name</label>
                     <input type="text" className='rounded-lg w-[400px] bg-[#7d8e95]' id="fullName" value={fullName} onChange={(e) => setFullName(e.target.value)} />
                 </div>
                 <div className="flex flex-col w-full items-center mb-10">
                     <label htmlFor="biography">Write Biography</label>
-                    <textarea id="biography" className="rounded-lg w-full h-52  bg-[#7d8e95]" value={biography} onChange={(e) => setBiography(e.target.value)} />
+                    <textarea id="biography" className="rounded-lg w-full h-52 bg-[#7d8e95]" value={biography} onChange={(e) => setBiography(e.target.value)} />
                 </div>
-                <div className="flex flex-col mb-10 font-bold" >
+                <div className="flex flex-col mb-10 font-bold">
                     <label htmlFor="image">Image</label>
                     <input className='bg-[#7d8e95]' type="file" id="image" accept="image/*" onChange={handleImageChange} />
                 </div>
